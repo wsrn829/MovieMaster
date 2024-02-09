3a97bf369f98d432e152f620f433143ca65389b0
wsrn829 <wsrn829@gmail.com>
2024-02-09 11:56:24 -0500

    Update url

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import SearchIcon from './search.svg';
 import MovieCard from './MovieCard';
 
 
-const API_URL = `http://www.omdbapi.com?apikey=${process.env.REACT_APP_API_KEY}&`;
+const API_URL = `https://www.omdbapi.com?apikey=${process.env.REACT_APP_API_KEY}&`;
 
 const App = () => {
     const [movies, setMovies] = useState([]);
