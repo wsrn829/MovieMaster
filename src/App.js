ee68646333fc63b9e0aeb8aa77ed2f1b7523fcd6
wsrn829 <wsrn829@gmail.com>
2024-02-09 11:47:39 -0500

    Set up deployment on Netlify

--- src/App.js
+++ src/App.js
@@ -1,12 +1,57 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import SearchIcon from './search.svg';
+import MovieCard from './MovieCard';
 
-// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+const API_URL = `http://www.omdbapi.com?apikey=${process.env.REACT_APP_API_KEY}&`;
 
 const App = () => {
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+        setMovies(data.Search);
+    }
+
+    useEffect(() => {
+        searchMovies("Love");
+    }, []);
+
+
     return (
-        <div>
-            <h1>App</h1>
-        </div>
+            <div className="app">
+                
+                <h1>MovieMaster</h1>
+                
+                <div className="search">
+                    <form onSubmit={(e) => {e.preventDefault(); searchMovies(searchTerm);}}>
+                        <input type="text" placeholder="Search for movies" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+                        <img src={SearchIcon} alt="search" onClick={() => searchMovies(searchTerm)} />
+                    </form>
+                </div>
+
+                {
+                    movies?.length > 0 
+                    ? (
+                        <div className="container">
+                            {movies.map((movie) => {
+                                return <MovieCard key={movie.imdbID} movie={movie} />
+                            })}
+                        </div>
+                    ) : (
+                        <div className="empty">
+                            <h2>No movies found</h2>
+                        </div>
+                    )
+                }
+
+                <div className="container">
+                </div>
+            
+            </div>
     );
 };
 
@@ -14,10 +59,3 @@ export default App;
 
 
 
-// <Router>
-        // <Switch>
-        //     <Route exact path="/" component={Home} />
-        //     <Route path="/about" component={About} />
-        //     <Route path="/contact" component={Contact} />
-        // </Switch>
-        // </Router>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
-import react from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+
+
+
+//The way React works is all React components are injected and rendered inside the root div here.
